29050acf179c3bf2b05544e569c989440f53ff26
Fernando <fernando_0970@outlook.es>
2018-04-09 02:23:49 +0000

    add mongoLab

--- app.js
+++ app.js
@@ -16,7 +16,9 @@ var commentRoutes    = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes      = require("./routes/index")
     
-mongoose.connect("mongodb://localhost/yelp_camp_v11");
+//mongoose.connect("mongodb://localhost/yelp_camp_v11");
+mongoose.connect("mongodb://fernando:fernando97@ds239359.mlab.com:39359/yelpcampdatabase");
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
